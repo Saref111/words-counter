a2f90cc197c9cc396a9541eb249ab809e89b8010
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-12-08 09:50:00 +0200

    added more features

---
+++ script.js
@@ -0,0 +1,16 @@
+// All undefined variables are in global scope from id attribute in HTML
+
+const unitToCountMethodsMap = {
+    'words': (text) => text.split(" ").filter((it) => it),
+    'characters': (text) => text.split("").filter((it) => it),
+    'letters': (text) => text.split("").filter((it) => it).filter((it) => it.match(/[a-zA-Z]/)),
+    'sentences': (text) => text.split(".").filter((it) => it),
+    'paragraphs': (text) => text.split("\n").filter((it) => it),
+    // 'all': (text) => text.split(" ").filter((it) => it).length + text.length + text.split(".").filter((it) => it).length + text.split("\n").filter((it) => it).length + text.split("\n").filter((it) => it).length,
+};
+
+countButton.addEventListener('click', () => {
+    console.log(unitToCount.value);
+    const r = unitToCountMethodsMap[unitToCount.value](textArea.value).length;
+      result.innerText = r + " " + unitToCount.value;
+});
